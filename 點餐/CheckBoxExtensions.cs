cc27dfb9fdbb4e94d6422b9c2b9bc4987ef2dbb7
MSI\佾庭 <ting3242006@hotmail.com>
2024-03-27 00:16:01 +0800

    Add extensions

---
+++ 點餐/CheckBoxExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 點餐
+{
+    public static class CheckBoxExtensions
+    {
+        public static int GetPrice(this CheckBox checkBox)
+        {
+            string text = checkBox.Text;
+            string number = Regex.Match(text, @"\d+").Value;
+            if (!string.IsNullOrEmpty(number))
+            {
+                return int.Parse(number);
+            }
+            return 0;
+        }
+    }
+}
---
+++ 點餐/FlowLayoutPanelExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 點餐
+{
+    public static class FlowLayoutPanelExtensions
+    {
+        public static void AddCheckBoxes(this FlowLayoutPanel panel, params string[] items)
+        {
+            foreach (string item in items)
+            {
+                CheckBox checkBox = new CheckBox();
+                checkBox.Text = item;
+                panel.Controls.Add(checkBox);
+            }
+        }
+
+        public static int GetTotalPrice(this FlowLayoutPanel panel)
+        {
+            int total = 0;
+            total += panel.Controls.OfType<CheckBox>().Where(cb => cb.Checked).Sum(cb => cb.GetPrice());
+            return total;
+        }
+    }
+}
